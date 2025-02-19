4377a119e2ef010c8ab111b8c50eccff46abef04
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-19 17:28:48 +0100

    VPC fix

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,19 @@
+resource "aws_vpc" "webapp_vpc" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "webapp_subnet_1" {
+  vpc_id            = aws_vpc.webapp_vpc.id
+  cidr_block        = "10.0.0.0/25"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "webapp_subnet_2" {
+  vpc_id            = aws_vpc.webapp_vpc.id
+  cidr_block        = "10.0.0.128/25"
+  availability_zone = "us-east-1b"
+}
+
 resource "aws_internet_gateway" "webapp_igw" {
   vpc_id = aws_vpc.webapp_vpc.id
 }
@@ -12,14 +28,4 @@ resource "aws_route" "webapp_route" {
   gateway_id             = aws_internet_gateway.webapp_igw.id
 }
 
-resource "aws_subnet" "webapp_subnet_1" {
-  vpc_id            = aws_vpc.webapp_vpc.id
-  cidr_block        = "10.0.0.0/25"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "webapp_subnet_2" {
-  vpc_id            = aws_vpc.webapp_vpc.id
-  cidr_block        = "10.0.0.128/25"
-  availability_zone = "us-east-1b"
 }
