0ff51baf3b10c00255bd5838fd8a96ae8cc62c16
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-21 13:38:24 +0100

    allow outbound connectivity

--- sg.tf
+++ sg.tf
@@ -28,6 +28,13 @@ resource "aws_security_group" "webapp_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "lambda_sg" {
