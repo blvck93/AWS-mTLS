e087c62ac87153a5cece8cae8eddf2f030a0232e
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-21 10:22:49 +0100

    allow ssh

--- sg.tf
+++ sg.tf
@@ -14,4 +14,11 @@ resource "aws_security_group" "webapp_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
